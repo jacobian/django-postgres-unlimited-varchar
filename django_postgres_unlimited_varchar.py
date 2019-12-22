8b77031366ce0ed5fcf0dfba748e5b14db47351c
Jacob Kaplan-Moss <jacob@jacobian.org>
2019-12-22 11:29:47 -0500

    🔖 1.1.0

--- django_postgres_unlimited_varchar.py
+++ django_postgres_unlimited_varchar.py
@@ -1,10 +1,9 @@
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 
 from django.db import models
 
 
 class UnlimitedCharField(models.CharField):
-
     def __init__(self, *args, **kwargs):
         # not a typo: we want to skip CharField.__init__ because that adds the max_length validator
         super(models.CharField, self).__init__(*args, **kwargs)
