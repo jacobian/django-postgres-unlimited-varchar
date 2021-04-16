17938f620c68bb9c1423d37087155b9b98976154
Craig <craig@boostinsurance.io>
2021-04-16 11:23:45 -0400

    Added db_collation support for Django >= 3.2 Increased patch version.

--- django_postgres_unlimited_varchar.py
+++ django_postgres_unlimited_varchar.py
@@ -1,10 +1,12 @@
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from django.db import models
 
 
 class UnlimitedCharField(models.CharField):
     def __init__(self, *args, **kwargs):
+        # update for Django >= 3.2
+        self.db_collation = kwargs.pop("db_collation", None)
         # not a typo: we want to skip CharField.__init__ because that adds the max_length validator
         super(models.CharField, self).__init__(*args, **kwargs)
 
